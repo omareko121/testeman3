f1b04e4dd2ec25095a5b83f17b7e4798d838fd36
omareko121 <130191693+omareko121@users.noreply.github.com>
2023-10-11 09:16:00 -0700

    Add files via upload

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php 
+
+$to = "ko601114@gmail.com";
+$title = "hi sir";
+$body = "iam omar";
+$header = "From: Support@wirless.co" . "\n" . "CC : ko601114@gmail.com";
+
+mail($to ,$title , $body ,$header );
+?>
